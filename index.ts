2f377d5f829f75df9135a6eb706ec606d3eb2f43
orestch <o.chuguivets@gmail.com>
2022-11-05 14:25:44 +0200

    Completed Section 2: Observable how does it work?

--- index.ts
+++ index.ts
@@ -3,8 +3,24 @@ import { Observable } from 'rxjs';
 const observable$ = new Observable<string>((subscriber) => {
   console.log('Observable executed');
   subscriber.next('Alice');
-  subscriber.next('Ben');
-  subscriber.next('Charlie');
+  setTimeout(() => subscriber.next('Ben'), 2000);
+  setTimeout(() => subscriber.next('Charlie'), 5000);
 });
 
-observable$.subscribe();
+const observer = {
+  next: (value) => console.log(`Observer 1: ${value}`),
+};
+
+const observer1 = {
+  next: (value) => console.log(`Observer 2: ${value}`),
+};
+
+const subscription = observable$.subscribe(observer);
+setTimeout(() => {
+  const subscription1 = observable$.subscribe(observer1);
+}, 2000);
+
+setTimeout(() => {
+  console.log('Ubsubscribe');
+  subscription.unsubscribe();
+}, 3000);
